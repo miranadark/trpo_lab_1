d0e2c59bdaa074da760ea0094d8922bdfb4edeaa
miranadark <57747648+miranadark@users.noreply.github.com>
2020-04-27 00:59:38 +0700

    Write code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
-
+#include <cstdio>
+#include "board_start.h"
+#include "board_print_plain.h"
+#include "boardgenerate.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    printf("Welcome to the game \"Chess\".\n"
+           "To do at least, enter in the console \"start position of the turn and end position of the move\"\n"
+           "Be sure to indicate the sequence through the dash.\n");
+    char deck[8][8];
+    board_start(deck);
+    while(1) {
+        board_print_plain(deck);
+        board(deck);
+    }
 }
